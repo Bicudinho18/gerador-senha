7c3976642f2749dbbb87814becc6cda8d56f391b
Bicudinho18 <marcus.santos.silva@escola.pr.gov.br>
2026-08-19 16:27:20 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
-
 const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@#$*';
+const forcaSenha = document.querySelector('.forca');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
@@ -12,7 +18,7 @@ function diminuiTamanho() {
     if (tamanhoSenha > 1){
         tamanhoSenha = tamanhoSenha - 1;
     }
-    
+   
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
@@ -21,22 +27,16 @@ function aumentaTamanho() {
     if (tamanhoSenha < 20){
         tamanhoSenha = tamanhoSenha + 1;
     }
-    
+   
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
 
-const campoSenha = document.querySelector('#campo-senha');
-const checkbox = document.querySelectorAll('.checkbox');
 for(i=0;i<checkbox.length;i++){
     checkbox[i].onclick = geraSenha;
 }
 
 
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
-const numeros = '0123456789';
-const simbolos = '!@#$*';
 geraSenha();
 
 function geraSenha(){
@@ -63,6 +63,20 @@ function geraSenha(){
         numeroAleatorio = Math.floor(numeroAleatorio);
         senha = senha + alfabeto[numeroAleatorio];
     }
-    
+   
     campoSenha.value = senha;
+    classificaSenha();
+}
+
+function classificaSenha(){
+    let entropia = tamanhoSenha*Math.log2(tamanhoAlfabeto);
+    forcaSenha.classList.remove('fraca', 'media', 'forte');
+    if(entropia > 57){
+        forcaSenha.classList.add('forte');
+    } else if (entropia > 35 && entropia < 57){
+        forcaSenha.classList.add('media');
+    } else if (entropia <= 35){
+        forcaSenha.classList.add('fraca');
+    }
+
 }
